705c476ab098b949fbd0a3903f1e26a3d2830bfc
Hunter Caron <huntercaron@mac.com>
2018-04-19 11:48:16 -0400

    updating packages & switching to a global css file

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,23 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
-import styled, { injectGlobal } from 'styled-components'
-import 'normalize.css'
-
-// importing fonts
-import NeueHaasGrotTextRoman_eot from "../assets/fonts/NeueHaasGrotText-55Roman-Web.eot"
-import NeueHaasGrotTextRoman_woff from "../assets/fonts/NeueHaasGrotText-55Roman-Web.woff"
-import NeueHaasGrotTextRoman_woff2 from "../assets/fonts/NeueHaasGrotText-55Roman-Web.woff2"
-import NeueHaasGrotTextItalic_eot from "../assets/fonts/NeueHaasGrotText-56Italic-Web.eot"
-import NeueHaasGrotTextItalic_woff from "../assets/fonts/NeueHaasGrotText-56Italic-Web.woff"
-import NeueHaasGrotTextItalic_woff2 from "../assets/fonts/NeueHaasGrotText-56Italic-Web.woff2"
-import NeueHaasGrotDispRoman_eot from "../assets/fonts/NeueHaasGrotDisp-55Roman-Web.eot"
-import NeueHaasGrotDispRoman_woff from "../assets/fonts/NeueHaasGrotDisp-55Roman-Web.woff"
-import NeueHaasGrotDispRoman_woff2 from "../assets/fonts/NeueHaasGrotDisp-55Roman-Web.woff2"
-import NeueHaasGrotDispItalic_eot from "../assets/fonts/NeueHaasGrotDisp-56Italic-Web.eot"
-import NeueHaasGrotDispItalic_woff from "../assets/fonts/NeueHaasGrotDisp-56Italic-Web.woff"
-import NeueHaasGrotDispItalic_woff2 from "../assets/fonts/NeueHaasGrotDisp-56Italic-Web.woff2"
-
+import styled from 'styled-components'
+import 'modern-normalize/modern-normalize.css'
+import './index.css'
 
 /*
   Layout File
@@ -72,16 +58,13 @@ const TemplateWrapper = ({ children }) => (
     <Helmet
       title="boiled"
       meta={[
-        { name: 'description', content: 'Sample' },
+        { name: 'description', content: 'boilerplate' },
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
 
     <Header />
-
-    {/*
-      Children() is where your page content is inserted.
-    */}
+    
     <InnerContainer>
       {children()}
     </InnerContainer>
@@ -94,93 +77,3 @@ TemplateWrapper.propTypes = {
 }
 
 export default TemplateWrapper
-
-
-/* Global Site Styles
- * used for importing fonts and setting up sizes
- * also for default values
- * avoid using for anything else
- */
-injectGlobal`
-  *, *:before, *:after {
-      box-sizing: border-box;
-      -webkit-overflow-scrolling: touch;
-  }
-
-  html {
-  ${'' /* Maybe Try?  font-size: calc(1.25vw + 62.5%); */}
-    font-size: 62.5%;
-    height: 100%;
-  }
-
-  body {
-      margin: 0;
-      height: 100%;
-
-      font-size: 1.6em;
-      line-height: 1.6;
-      font-weight: 400;
-      font-family: 'Haas Grot Text', 'Helvetica', 'Arial', sans-serif;
-      color: #222;
-      webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
-      -ms-text-size-adjust: 100%;
-      -webkit-text-size-adjust: 100%;
-      text-rendering: optimizeLegibility;
-  }
-
-  #___gatsby {
-    height: 100%;
-  }
-
-  h1, h2, h3, h4, h5, h6 {
-    font-family: 'Haas Grot Display', 'Helvetica', 'Arial', sans-serif;
-    margin-top: 0;
-    margin-bottom: 0rem;
-    font-weight: 400;
-  }
-
-  @font-face {
-    font-family: 'Haas Grot Text';
-    src: url(${NeueHaasGrotTextRoman_eot});
-    src: url('${NeueHaasGrotTextRoman_eot}?#iefix') format('embedded-opentype'),
-      url('${NeueHaasGrotTextRoman_woff2}') format('woff2'),
-      url('${NeueHaasGrotTextRoman_woff}') format('woff');
-    font-weight: 400;
-    font-style: normal;
-    font-stretch: normal;
-  }
-
-  @font-face {
-    font-family: 'Haas Grot Text';
-    src: url(${NeueHaasGrotTextRoman_eot});
-    src: url('${NeueHaasGrotTextItalic_eot}?#iefix') format('embedded-opentype'),
-      url('${NeueHaasGrotTextItalic_woff2}') format('woff2'),
-      url('${NeueHaasGrotTextItalic_woff}') format('woff');
-    font-weight: 400;
-    font-style: italic;
-    font-stretch: normal;
-  }
-
-  @font-face {
-    font-family: 'Haas Grot Display';
-    src: url(${NeueHaasGrotDispRoman_eot});
-    src: url('${NeueHaasGrotDispRoman_eot}?#iefix') format('embedded-opentype'),
-      url('${NeueHaasGrotDispRoman_woff2}') format('woff2'),
-      url('${NeueHaasGrotDispRoman_woff}') format('woff');
-    font-weight: 400;
-    font-style: normal;
-    font-stretch: normal;
-  }
-
-  @font-face {
-    font-family: 'Haas Grot Display';
-    src: url(${NeueHaasGrotDispItalic_eot});
-    src: url('${NeueHaasGrotDispItalic_eot}?#iefix') format('embedded-opentype'),
-      url('${NeueHaasGrotDispItalic_woff2}') format('woff2'),
-      url('${NeueHaasGrotDispItalic_woff}') format('woff');
-    font-weight: 400;
-    font-style: italic;
-    font-stretch: normal;
-  }
-`
